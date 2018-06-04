7de5c43caaced9d1ca4d3bb913cb59bbdc63ccf1
Corey Castillo <mcguiser@users.noreply.github.com>
2018-06-03 21:24:38 -0500

    Extracts URL, id, name, and price for each item. Generates webpage using the data.
    Reads text file containing HTML/CSS for the first and second half of the HTML document.  Generates an HTML table based on item data.  Put all HTML/CSS into a string which is written to a single HTML file.

--- store-html-updater.py
+++ store-html-updater.py
@@ -34,10 +34,48 @@ with requests.Session() as s:
         f.write(str(r.content))
         f.close()
 
-        for x in soup.find_all('a'):
+        item_count = 0
+        table_string = '<table><tr>'
+        for a in soup.find_all('a'):
             try:
-                if 'Are you sure you wish to buy' in x['onclick']:
-                    print(x['onclick'])
+                if 'Are you sure you wish to buy' in a['onclick']:
+                    img_url = a.find_all('img')[0]['src']
+                    obj_info_id = re.findall(r'obj_info_id=(\d+)&', a['href'])[0]
+                    item_name_price = re.findall(r'Are you sure you wish to buy (.*?) at (.*?)\?', a['onclick'])
+                    item_name = item_name_price[0][0]
+                    item_price = item_name_price[0][1]
+                    item_price_int = item_price.split()[0].replace(',', '')
+
+                    if item_count == 4:
+                        table_string += '</tr>'
+                        item_count = 0
+
+                    table_string += '<td class=\"item_cell\">'
+                    table_string += '<a href=\"http://www.neopets.com/browseshop.phtml?owner=' + username + '&buy_obj_info_id=' + obj_info_id + '&buy_cost_neopoints=' + item_price_int + '\">'
+                    table_string += '<img alt=\"' + item_name + '" src=\"' + img_url + '\">'
+                    table_string += '<br> ' + item_name
+                    table_string += '<br> ' + item_price
+                    table_string += '</a></td>'
+                    
+                    item_count += 1
+
+                    print(obj_info_id)
+                    print(img_url)
+                    print(item_name)
+                    print(item_price)
             except:pass
+        if item_count <= 4:
+            table_string += '<td class=\"item_cell\"></td>'*(4-item_count)
+        table_string += '</tr></table>'
+        print(table_string)
+
+        print()
+        with open('X:\\User\\Documents\\store-html-updater\\first-half.txt', 'r') as myfile:
+            first_half=myfile.read()
+        with open('X:\\User\Documents\\store-html-updater\\second-half.txt', 'r') as myfile:
+            second_half=myfile.read()
+        with open('X:\\User\\Documents\\store-html-updater\\final.html', 'w') as myfile:
+            myfile.write(first_half + table_string + second_half)
+
     else:
         print('Failed to log in.', flush=True)
